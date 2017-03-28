281923f1318942bef583db3ccb8e6258153dea6b
Riley Bittle <rbittle34@gmail.com>
2017-03-27 19:45:29 -0500

    input parsing and initial solving logic

--- prover.py
+++ prover.py
@@ -6,23 +6,62 @@ from queue import Queue
 
 # CLI
 parser = argparse.ArgumentParser()
-parser.add_argument("in", action="store")
+parser.add_argument("input", action="store")
 args = parser.parse_args()
 
-parse_kb(f):
-    return ()
+
+
+def parse_kb(f):
+    kb = []
+    for line in f.readlines():
+        clause = []
+        # clean up newlines
+        line = line.strip()
+
+        # split by whitespace
+        var_array = line.split(" ")
+        for var in var_array:
+            if var.find("~") == 0:
+                # negated values
+                var_name = var[1:]
+                clause.append((var_name, False))
+            else:
+                # positive values
+                clause.append((var, True))
+        kb.append(clause)
+
+    return tuple(kb)
+
+    def unifiable(c1, c2):
+        return False
+
+    def unify(c1, c2):
+        return []
 
 class Solver:
     def __init__(self, knowlege_base):
         self.kb = knowlege_base
 
+    def solve(self):
+        print(self.kb)
 
+        kb = self.kb
 
+        while True: 
+            # try to unify clauses in the kb
+            for clause1 in kb:
+                for clause2 in kb:
+                    if unifiable(clause1, clause2):
+                        result = unify(clause1, clause2)
+                        if result != []:
+                            kb.append(result)
+                        else:
+                            break
 
 
 
 def main():
-    with f as open(args.in, "r"):
+    with open(args.input, "r") as f:
 
         knowlege_base = parse_kb(f)
 
